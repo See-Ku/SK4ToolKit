7734efd17cc598cfddce0d8e86bc532ce9425f1c
See.Ku <info@see-ku.com>
2016-03-23 19:05:38 +0900

    Initial Commit

---
+++ SK4Toolkit/SK4Toolkit.h
@@ -0,0 +1,19 @@
+//
+//  SK4Toolkit.h
+//  SK4Toolkit
+//
+//  Created by See.Ku on 2016/03/23.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SK4Toolkit.
+FOUNDATION_EXPORT double SK4ToolkitVersionNumber;
+
+//! Project version string for SK4Toolkit.
+FOUNDATION_EXPORT const unsigned char SK4ToolkitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SK4Toolkit/PublicHeader.h>
+
+
